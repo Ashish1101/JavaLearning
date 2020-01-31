5c3e3b9c2622faa89276d3f2498fc7108f6a2ba0
Ashish <ashishskkumar321@gmail.com>
2020-01-31 09:39:04 +0530

    Enumator

--- Enumator.java
+++ Enumator.java
@@ -5,11 +5,14 @@ it can be declared inside the class as well as outside the class*/
 enum dir {East, West, North, South}
 class Enumator {
   
+//enumator as a consturctor
 enum dress {jeans(1), Pant(2), Shirt(3);
 private int x;
 private dress(int y) {
     x = y;
 }}
+
+
 public static void main(String[] args) {
     // for ( dir d : dir.values()) {
     //     System.out.println(d);
